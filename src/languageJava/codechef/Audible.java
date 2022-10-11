ee086030c453ce6dc737d25fc7fb4c68385b4607
RashmikantaPanda <rashmikantapanda1234@gmail.com>
2022-10-11 22:52:21 +0530

    ProblemCode:AUDIBLE ContestCode:START59

---
+++ src/languageJava/codechef/Audible.java
@@ -0,0 +1,36 @@
+/*
+    Chef's dog binary hears frequencies starting from 6767 Hertz to 4500045000 Hertz (both inclusive).
+    If Chef's commands have a frequency of XX Hertz, find whether binary can hear them or not.
+
+Input Format
+    The first line of input will contain a single integer TT, denoting the number of test cases.
+    Each test case consists of a single integer XX - the frequency of Chef's commands in Hertz.
+Output Format
+    For each test case, output on a new line YES, if binary can hear Chef's commands. Otherwise, print NO.
+    The output is case-insensitive. Thus, the strings YES, yes, yeS, and Yes are all considered the same.
+    */
+
+package languageJava.codechef;
+import java.util.Scanner;
+
+public class Audible
+{
+    public static void main (String[] args)
+    {
+        int t;
+        Scanner sc=new Scanner(System.in);
+        t=sc.nextInt();
+        while(t>0)
+        {
+            int x=sc.nextInt();
+            if(x>=67 && x<=45000)
+            {
+                System.out.println("YES");
+            }
+            else{
+                System.out.println("NO");
+            }
+            t--;
+        }
+    }
+}
