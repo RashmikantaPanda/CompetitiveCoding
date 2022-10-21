4a058a4a12f712d818d7881e6c585b074852701f
RashmikantaPanda <rashmikantapanda1234@gmail.com>
2022-10-21 23:26:15 +0530

    ProblemCode: Acuracy ContestCode:

--- src/languageJava/codechef/Accuracy.java
+++ src/languageJava/codechef/Accuracy.java
@@ -1,5 +1,6 @@
 
-    /*Problem
+    /*
+    Problem
         There are 100 questions in a paper. Each question carries +3 marks for correct answer, -1 marks for incorrect answer and 0 marks for unattempted question.
 
         It is given that Chef received exactly X(0 <= X <= 100 marks. Determine the minimum number of problems Chef marked incorrect.
