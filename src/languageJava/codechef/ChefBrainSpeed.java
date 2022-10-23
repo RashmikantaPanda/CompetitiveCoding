dede1d1d2616917f02a2329d3c81d5f12d4ef6ef
RashmikantaPanda <rashmikantapanda1234@gmail.com>
2022-10-23 17:12:44 +0530

    ProblemCode: BrainSpeed  ContestCode: DR-288

---
+++ src/languageJava/codechef/ChefBrainSpeed.java
@@ -0,0 +1,22 @@
+package languageJava.codechef;
+
+import java.util.*;
+import java.lang.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class ChefBrainSpeed
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        // your code goes here
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        if(y>x){
+            System.out.println("YES");
+        }
+        else{
+            System.out.println("NO");
+        }
+    }
+}
