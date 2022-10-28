16dcfe30183779625e9ae83f3d8cc96df6896fac
RashmikantaPanda <rashmikantapanda1234@gmail.com>
2022-10-29 01:04:43 +0530

    ProblemCode: MAXDIFFMIN ContestCode:START62 DR-339

--- src/languageJava/codechef/MaxDiffMin.java
+++ src/languageJava/codechef/MaxDiffMin.java
@@ -1,3 +1,20 @@
+
+/*      Problem Code: MAXDIFFMIN    Contest Code:START62   Difficulty Rating:339
+
+Problem
+        Chef is given 33 integers A, B and C such that A < B < C.
+
+        Chef needs to find the value of max(A, B, C) - min(A, B, C).
+
+        Here max(A, B, C) denotes the maximum value among A, B, C while min(A, B, C) denotes the minimum value among A, B,C.
+
+        Input Format
+        The first line of input will contain a single integer T, denoting the number of test cases.
+        Each test case consists of 3 integers A, B,C.
+        Output Format
+        For each test case, output the value of max(A, B, C) - min(A, B, C).
+
+*/
 package languageJava.codechef;
 
 import java.util.*;
